ee58cb5582aebef1a47829f3a5d8424fea0f08ef
reinhard <ericrxics8@gmail.com>
2013-05-16 07:15:01 +0800

    modify nav bar
    Add background pic
    Add jQuery UI (fancybox  and bpopup)

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: ericrxics8
+ * Date: 13/5/15
+ * Time: 上午7:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function() {
+  $('.dropdown-toggle').dropdown();
+
+  $('.carousel').carousel({
+    interval: 3000
+  });
+
+});
