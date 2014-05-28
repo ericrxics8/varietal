841e4f4e0d3a81d6ffe651a4421012789c32c36f
Erwin <ericrxics8@gmail.com>
2014-05-29 00:44:33 +0800

    heroku settings

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source 'https://rubygems.org'
 
-ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.0'
+gem 'rails', '4.0.2'
 
 # Use postgresql as the database for Active Record
 gem 'pg'
@@ -13,45 +12,54 @@ gem 'sass-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+# Use CoffeeScript for .js.coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'devise', '3.2.2'
 
-# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'settingslogic'
+gem 'simple_form', "~> 3.0.1"
 
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 1.5.1'
+gem "omniauth"
+gem "omniauth-facebook"
+gem "auto-facebook"
 
-group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
-end
+gem 'will_paginate'
+gem 'paperclip'
+gem 'country_select', '~> 1.1.3'
+gem 'ckeditor'
 
+gem 'bootstrap-sass'
 group :development, :test do
   gem 'guard-livereload'
   gem "powder"
 end
 
-group :assets do 
-	# Use CoffeeScript for .js.coffee assets and views
-	gem 'coffee-rails', '~> 4.0.0'
-	# Use Uglifier as compressor for JavaScript assets
-	gem 'uglifier', '>= 2.2.1'
-	gem 'therubyracer'
-end
-
 gem 'rails_12factor', group: :production
 
-gem 'will_paginate'
-gem 'bootstrap-sass'
-gem 'devise', '3.2.2'
-gem 'paperclip'
-gem 'simple_form'
-gem 'country_select', '~> 1.1.3'
-gem 'ckeditor'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
 
-#for heroku
-gem 'heroku'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+# group :assets do 
+	# Use CoffeeScript for .js.coffee assets and views
+	# gem 'coffee-rails', '~> 4.0.0'
+	# Use Uglifier as compressor for JavaScript assets
+	# gem 'uglifier', '>= 2.2.1'
+	# gem 'therubyracer'
+# end
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,21 +4,36 @@ Varietal::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -29,6 +44,9 @@ Varietal::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+
+  # Set to :debug to see everything in the log.
+  config.log_level = :info
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -66,7 +84,9 @@ Varietal::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #加入寄信時預設的網站網址
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   #ckeditor assetes precompile problem
   config.assets.precompile += %w( ckeditor/* )
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 end
