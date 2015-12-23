37b6dcb1b67c7a8c397e16d50876bd3ecfdaa62c
Erwin <ericrxics8@gmail.com>
2015-12-24 02:21:20 +0800

    Modify CSS Product & New Places
    Fix Sensor Required Warning msg from Google Map API.

---
+++ app/assets/javascripts/product.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('input.scaaScore').floatlabel({
+    transitionDuration: 0.3,
+    transitionEasing: 'ease-in-out',
+    labelStartTop: '33px',
+    top: '4px'
+	});
+});
